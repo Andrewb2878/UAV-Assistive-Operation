92f23be9d950cdb1ca065cb8c80357488d632f7a
Andrew Bradley <100674957@unimail.derby.ac.uk>
2026-01-14 19:00:31 +0000

    Added bluetooth controller detection - supporting both standard inputs and additional inputs from adaptive controllers

--- UAV-Assistive-Operation/MainPage.xaml.cs
+++ UAV-Assistive-Operation/MainPage.xaml.cs
@@ -3,9 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Gaming.Input;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,12 +26,36 @@ namespace UAV_Assistive_Operation
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Gamepad _gamepad;
+        private RawGameController _rawGameController;
+        private readonly DispatcherTimer _inputUpdateTimer;
+
+
         public MainPage()
         {
             this.InitializeComponent();
-            DJISDKManager.Instance.SDKRegistrationStateChanged += Instance_SDKRegistrationEvent;
 
+            //DJI setup
+            DJISDKManager.Instance.SDKRegistrationStateChanged += Instance_SDKRegistrationEvent;
             DJISDKManager.Instance.RegisterApp("7b980d8aa60b87f6b740fd94");
+
+
+            //Gamepad setup
+            Gamepad.GamepadAdded += Instance_GamepadAdded;
+            Gamepad.GamepadRemoved += Instance_GamepadRemoved;
+            //Handles non-standard peripherals or additional inputs
+            RawGameController.RawGameControllerAdded += Instance_RawControllerAdded;
+            RawGameController.RawGameControllerRemoved += Instance_RawControllerRemoved;
+
+            if (Gamepad.Gamepads.Count > 0)
+            {
+                _gamepad = Gamepad.Gamepads[0];
+            }
+
+            _inputUpdateTimer = new DispatcherTimer();
+            _inputUpdateTimer.Interval = TimeSpan.FromMilliseconds(20);
+            _inputUpdateTimer.Tick += Instance_InputUpdate;
+            _inputUpdateTimer.Start();
         }
 
         private async void Instance_SDKRegistrationEvent(SDKRegistrationState state, SDKError resultCode)
@@ -68,6 +95,86 @@ namespace UAV_Assistive_Operation
             {
                 System.Diagnostics.Debug.WriteLine("Resgister SDK failed, the error is: ");
                 System.Diagnostics.Debug.WriteLine(resultCode.ToString());
+            }
+        }
+
+
+        private void Instance_GamepadAdded(object sender, Gamepad gamepad)
+        {
+            _gamepad = gamepad;
+            System.Diagnostics.Debug.WriteLine("Gamepad now connected.");
+        }
+
+        private void Instance_GamepadRemoved(object sender, Gamepad gamepad)
+        {
+            if (_gamepad == gamepad)
+            {
+                _gamepad = null;
+                System.Diagnostics.Debug.WriteLine("Gamepad now disconnected.");
+            }
+        }
+
+        private void Instance_RawControllerAdded(object sender, RawGameController controller)
+        {
+            _rawGameController = controller;
+            System.Diagnostics.Debug.WriteLine("Raw controller now connected.");
+        }
+
+        private void Instance_RawControllerRemoved(object sender, RawGameController controller)
+        {
+            if (_rawGameController == controller)
+            {
+                _rawGameController = null;
+                System.Diagnostics.Debug.WriteLine("Raw controller now disconnected.");
+            }
+        }
+
+        //Reading input from gamepad/controller
+        private void Instance_InputUpdate(object sender, object gamepad)
+        {
+            //Standard gamepad input
+            if (_gamepad == null)
+            {
+                return;
+            }
+                
+            var reading = _gamepad.GetCurrentReading();
+
+            //Joysticks
+            double leftX = reading.LeftThumbstickX;
+            double leftY = reading.LeftThumbstickY;
+            double rightX = reading.RightThumbstickX;
+            double rightY = reading.RightThumbstickY;
+
+            //Triggers
+            double leftTrigger = reading.LeftTrigger;
+            double rightTrigger = reading.RightTrigger;
+
+            //Buttons
+            if (reading.Buttons.HasFlag(GamepadButtons.A))
+            {
+                
+            }
+            if (reading.Buttons.HasFlag(GamepadButtons.B))
+            {
+
+            }
+
+            //Raw controller input
+            if (_rawGameController == null)
+            {
+                return;
+            }
+
+            var buttons = new bool[_rawGameController.ButtonCount];
+            var switches = new GameControllerSwitchPosition[_rawGameController.SwitchCount];
+            var axis = new double[_rawGameController.AxisCount];
+
+            _rawGameController.GetCurrentReading(buttons, switches, axis);
+
+            if (buttons.Length > 0 && buttons[0])
+            {
+
             }
         }
     }
